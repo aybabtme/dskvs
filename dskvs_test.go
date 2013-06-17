8d4f5465a329c77ce618d9953576a296e2f6903d
Antoine Grondin <antoinegrondin@gmail.com>
2013-06-16 20:24:19 -0400

    Test for proper working of map in single-thread context.

--- dskvs_test.go
+++ dskvs_test.go
@@ -3,14 +3,19 @@ package dskvs
 import (
 	"bytes"
 	"encoding/json"
+	"strconv"
 	"testing"
 )
 
-type TestData struct {
+type Data struct {
 	Descr string
 }
 
-func TestCreatingStore(t *testing.T) {
+/*
+	Boilerplate
+*/
+
+func setUp(t *testing.T) *Store {
 	store, err := NewStore("./db")
 	if err != nil {
 		t.Fatalf("Error creating store", err)
@@ -20,33 +25,47 @@ func TestCreatingStore(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error loading store", err)
 	}
+	return store
+}
 
-	err = store.Close()
+func tearDown(store *Store, t *testing.T) {
+	err := store.Close()
 	if err != nil {
 		t.Fatalf("Error closing store", err)
 	}
 }
 
-func TestGet(t *testing.T) {
-
-	store, err := NewStore("./db")
-	if err != nil {
-		t.Fatalf("Error creating store", err)
-	}
-
-	err = store.Load()
-	if err != nil {
-		t.Fatalf("Error loading store", err)
-	}
-
-	expected, err := json.Marshal(TestData{"the great and epics"})
+func getData(d Data, t *testing.T) []byte {
+	dataBytes, err := json.Marshal(d)
 	if err != nil {
 		t.Fatal("Error with test data", err)
 	}
+	return dataBytes
+}
 
-	store.Put("artist/daftpunk", expected)
+/*
+	Test cases
+*/
 
-	actual, err := store.Get("artist/daftpunk")
+func TestCreatingStore(t *testing.T) {
+	store := setUp(t)
+	tearDown(store, t)
+}
+
+func TestSingleOperation(t *testing.T) {
+
+	store := setUp(t)
+	defer tearDown(store, t)
+
+	key := "artist/daftpunk"
+	expected := getData(Data{"The peak of awesome"}, t)
+
+	err := store.Put(key, expected)
+	if err != nil {
+		t.Fatalf("Error putting data in", err)
+	}
+
+	actual, err := store.Get(key)
 	if err != nil {
 		t.Fatalf("Error getting data back", err)
 	}
@@ -57,8 +76,68 @@ func TestGet(t *testing.T) {
 			actual)
 	}
 
-	err = store.Close()
+	err = store.Delete(key)
 	if err != nil {
-		t.Fatalf("Error closing store", err)
+		t.Fatalf("Error deleting data we just put", err)
+	}
+	_, err = store.Get(key)
+	if err == nil {
+		t.Fatalf("Expected to receive KeyError but no error")
+	}
+	switch err := err.(type) {
+	case KeyError:
+		// Expected
+		break
+	default:
+		t.Fatalf("Expected to receive KeyError but got <%s>", err)
+	}
+
+}
+
+func TestMultipleOperations(t *testing.T) {
+
+	store := setUp(t)
+	defer tearDown(store, t)
+
+	var key string
+	var expected []byte
+	var actual []byte
+	for i := int(0); i < 10; i++ {
+		key = "artist/daftpunk" + strconv.Itoa(i)
+		expected = getData(Data{"The peak of awesome" + strconv.Itoa(i)}, t)
+
+		err := store.Put(key, expected)
+		if err != nil {
+			t.Errorf("Error putting data in", err)
+		}
+		actual, err = store.Get(key)
+		if err != nil {
+			t.Errorf("Error getting data back", err)
+		}
+
+		if !bytes.Equal(expected, actual) {
+			t.Errorf("Expected <%s> but was <%s>",
+				expected,
+				actual)
+		}
+	}
+
+	err := store.DeleteAll("artist")
+	if err != nil {
+		t.Fatal("Error deleting key", err)
+	}
+	for i := int(0); i < 10; i++ {
+		key = "artist/daftpunk" + strconv.Itoa(i)
+		_, err = store.Get(key)
+		if err == nil {
+			t.Fatalf("Expected to receive KeyError, but got no error at all")
+		}
+		switch err := err.(type) {
+		case KeyError:
+			// Expected
+			break
+		default:
+			t.Fatalf("Expected to receive KeyError but got <%s>", err)
+		}
 	}
 }
