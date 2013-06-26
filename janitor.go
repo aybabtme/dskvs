51aa5bd5a8f7467174893f05fb78d41b5d732262
Antoine Grondin <antoinegrondin@gmail.com>
2013-06-26 00:37:54 -0400

    Implement conditional selects to prioritize tasks.

--- janitor.go
+++ janitor.go
@@ -32,17 +32,40 @@ func (j *janitor) unloadStore(s *Store) error {
 	return nil
 }
 
+func (j *janitor) dirtyPageIfNoMember() chan *page {
+	if len(j.ToCreate) != 0 {
+		return nil
+	}
+	if len(j.ToDelete) != 0 {
+		return nil
+	}
+	return j.DirtyPages
+}
+
+func (j *janitor) shouldDie() chan bool {
+	if len(j.ToCreate) != 0 {
+		return nil
+	}
+	if len(j.ToDelete) != 0 {
+		return nil
+	}
+	if len(j.DirtyPages) != 0 {
+		return nil
+	}
+	return j.mustDie
+}
+
 func (j *janitor) run() {
 	go func() {
 		for {
 			select {
-			case dirty := <-j.DirtyPages:
+			case dirty := <-j.dirtyPageIfNoMember():
 				writeToFile(dirty)
 			case delete := <-j.ToDelete:
 				deleteFolder(delete)
 			case create := <-j.ToCreate:
 				createFolder(create)
-			case <-j.mustDie:
+			case <-j.shouldDie():
 				log.Printf("janitor dying")
 				return
 			}
