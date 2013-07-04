33e36428fe5419af5ad8723de4d3577104b62746
Antoine Grondin <antoinegrondin@gmail.com>
2013-07-04 03:37:06 -0400

    Clean artifact after pprof.

--- do_pprof.sh
+++ do_pprof.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 go test -cpuprofile doc/cpu.pprof && go tool pprof --svg dskvs.test doc/cpu.pprof > doc/cpu_profile.svg
 go test -memprofile doc/mem.pprof && go tool pprof --svg dskvs.test doc/mem.pprof > doc/mem_profile.svg
+rm dskvs.test
