1e6cd8ac216047df086f70ee8483af0239cb09bf
Antoine Grondin <antoinegrondin@gmail.com>
2013-07-04 20:43:56 -0400

    CHange doc folder structure.

--- gocov.bash
+++ gocov.bash
@@ -1,2 +1,4 @@
 #!/bin/sh
+set -e
+mkdir -p doc/cov/
 gocov test | gocov-html > doc/cov/dskvs.html
--- pprof.bash
+++ pprof.bash
@@ -1,6 +1,8 @@
 #! /bin/sh
-go test -cpuprofile doc/cpu.pprof
-go test -memprofile doc/mem.pprof
-go tool pprof --svg dskvs.test doc/mem.pprof > doc/mem_profile.svg
-go tool pprof --svg dskvs.test doc/cpu.pprof > doc/cpu_profile.svg
+set -e
+mkdir -p doc/pprof/
+go test -cpuprofile doc/pprof/cpu.pprof
+go test -memprofile doc/pprof/mem.pprof
+go tool pprof --svg dskvs.test doc/pprof/mem.pprof > doc/pprof/mem_profile.svg
+go tool pprof --svg dskvs.test doc/pprof/cpu.pprof > doc/pprof/cpu_profile.svg
 rm dskvs.test
